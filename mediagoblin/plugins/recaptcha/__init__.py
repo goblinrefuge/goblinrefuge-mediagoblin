6de1a1b15b51d3726cc6b9bfadb3ff4f54e54a69
Goblin Refuge <mediagoblin@goblinrefuge.com>
2014-11-01 00:00:01 -0400

    Adding recaptcha plugin

---
+++ mediagoblin/plugins/recaptcha/__init__.py
@@ -0,0 +1,145 @@
+# GNU MediaGoblin -- federated, autonomous media hosting
+# Copyright (C) 2011, 2012 MediaGoblin contributors.  See AUTHORS.
+#
+# This program is free software: you can redistribute it and/or modify
+# it under the terms of the GNU Affero General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
+#
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU Affero General Public License for more details.
+#
+# You should have received a copy of the GNU Affero General Public License
+# along with this program.  If not, see <http://www.gnu.org/licenses/>.
+from pkg_resources import resource_filename
+import os
+import logging
+
+from mediagoblin.plugins.recaptcha import forms as auth_forms
+from mediagoblin.plugins.basic_auth import tools as auth_tools
+from mediagoblin.auth.tools import create_basic_user
+from mediagoblin.db.models import User
+from mediagoblin.tools import pluginapi
+from sqlalchemy import or_
+from mediagoblin.tools.staticdirect import PluginStatic
+_log = logging.getLogger(__name__)
+
+
+PLUGIN_DIR = os.path.dirname(__file__)
+
+
+def setup_plugin():
+    _log.info('Setting up recaptcha...')
+    config = pluginapi.get_config('mediagoblin.plugins.recaptcha')
+    if config:
+        if config.get('RECAPTCHA_USE_SSL') == True:
+            _log.info('reCAPTCHA is configured to use SSL.')
+        else:
+            _log.info('reCAPTCHA is NOT configured to use SSL.')
+
+        if config.get('RECAPTCHA_PUBLIC_KEY') == 'domainpublickey':
+            _log.warn('reCAPTCHA public key was not specified.')
+        if config.get('RECAPTCHA_PRIVATE_KEY') == 'domainprivatekey':
+            _log.warn('reCAPTCHA private key was not specified.')
+
+    routes = [
+        ('mediagoblin.plugins.recaptcha.register',
+         '/auth/recaptcha/register/',
+         'mediagoblin.plugins.recaptcha.views:register'),
+        ('mediagoblin.plugins.recaptcha.login',
+         '/auth/recaptcha/login/',
+         'mediagoblin.plugins.recaptcha.views:login'),
+        ('mediagoblin.plugins.recaptcha.edit.pass',
+         '/edit/password/',
+         'mediagoblin.plugins.recaptcha.views:change_pass'),
+        ('mediagoblin.plugins.recaptcha.forgot_password',
+         '/auth/forgot_password/',
+         'mediagoblin.plugins.recaptcha.views:forgot_password'),
+        ('mediagoblin.plugins.recaptcha.verify_forgot_password',
+         '/auth/forgot_password/verify/',
+         'mediagoblin.plugins.recaptcha.views:verify_forgot_password')]
+
+    pluginapi.register_routes(routes)
+    pluginapi.register_template_path(os.path.join(PLUGIN_DIR, 'templates'))
+
+    pluginapi.register_template_hooks(
+        {'edit_link': 'mediagoblin/plugins/recaptcha/edit_link.html',
+         'fp_link': 'mediagoblin/plugins/recaptcha/fp_link.html',
+         'fp_head': 'mediagoblin/plugins/recaptcha/fp_head.html',
+         'create_account':
+            'mediagoblin/plugins/recaptcha/create_account_link.html'})
+
+    _log.info('Done setting up recaptcha!')
+
+
+def get_user(**kwargs):
+    username = kwargs.pop('username', None)
+    if username:
+        user = User.query.filter(
+            or_(
+                User.username == username,
+                User.email == username,
+            )).first()
+        return user
+
+
+def create_user(registration_form):
+    user = get_user(username=registration_form.username.data)
+    if not user and 'password' in registration_form:
+        user = create_basic_user(registration_form)
+        user.pw_hash = gen_password_hash(
+            registration_form.password.data)
+        user.save()
+    return user
+
+
+def get_login_form(request):
+    return auth_forms.LoginForm(request.form)
+
+
+def get_registration_form(request):
+    return auth_forms.RegistrationForm(request.form)
+
+
+def gen_password_hash(raw_pass, extra_salt=None):
+    return auth_tools.bcrypt_gen_password_hash(raw_pass, extra_salt)
+
+
+def check_password(raw_pass, stored_hash, extra_salt=None):
+    if stored_hash:
+        return auth_tools.bcrypt_check_password(raw_pass,
+                                                stored_hash, extra_salt)
+    return None
+
+
+def no_pass_redirect():
+    return 'recaptcha'
+
+
+def auth():
+    return True
+
+
+def append_to_global_context(context):
+    context['pass_auth'] = True
+    return context
+
+
+hooks = {
+    'setup': setup_plugin,
+    'authentication': auth,
+    'auth_get_user': get_user,
+    'auth_create_user': create_user,
+    'auth_get_login_form': get_login_form,
+    'auth_get_registration_form': get_registration_form,
+    'auth_no_pass_redirect': no_pass_redirect,
+    'auth_gen_password_hash': gen_password_hash,
+    'auth_check_password': check_password,
+    'auth_fake_login_attempt': auth_tools.fake_login_attempt,
+    #'template_global_context': append_to_global_context,
+    'static_setup': lambda: PluginStatic(
+        'coreplugin_recaptcha',
+        resource_filename('mediagoblin.plugins.recaptcha', 'static'))
+}
---
+++ mediagoblin/plugins/recaptcha/forms.py
@@ -0,0 +1,78 @@
+# GNU MediaGoblin -- federated, autonomous media hosting
+# Copyright (C) 2011, 2012 MediaGoblin contributors.  See AUTHORS.
+#
+# This program is free software: you can redistribute it and/or modify
+# it under the terms of the GNU Affero General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
+#
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU Affero General Public License for more details.
+#
+# You should have received a copy of the GNU Affero General Public License
+# along with this program.  If not, see <http://www.gnu.org/licenses/>.
+import wtforms
+
+from mediagoblin.tools.translate import lazy_pass_to_ugettext as _
+from mediagoblin.auth.tools import normalize_user_or_email_field
+
+#from wtfrecaptcha.fields import RecaptchaField
+
+class RegistrationForm(wtforms.Form):
+    username = wtforms.StringField(
+        _('Username'),
+        [wtforms.validators.Required(),
+         normalize_user_or_email_field(allow_email=False)])
+    password = wtforms.PasswordField(
+        _('Password'),
+        [wtforms.validators.Required(),
+         wtforms.validators.Length(min=5, max=1024)])
+    email = wtforms.StringField(
+        _('Email address'),
+        [wtforms.validators.Required(),
+         normalize_user_or_email_field(allow_user=False)])
+
+
+class LoginForm(wtforms.Form):
+    username = wtforms.StringField(
+        _('Username or Email'),
+        [wtforms.validators.Required(),
+         normalize_user_or_email_field()])
+    password = wtforms.PasswordField(
+        _('Password'),
+        [wtforms.validators.Required()])
+    stay_logged_in = wtforms.BooleanField(
+        label='',
+        description=_('Stay logged in'))
+
+
+class ForgotPassForm(wtforms.Form):
+    username = wtforms.StringField(
+        _('Username or email'),
+        [wtforms.validators.Required(),
+         normalize_user_or_email_field()])
+
+
+class ChangeForgotPassForm(wtforms.Form):
+    password = wtforms.PasswordField(
+        'Password',
+        [wtforms.validators.Required(),
+         wtforms.validators.Length(min=5, max=1024)])
+    token = wtforms.HiddenField(
+        '',
+        [wtforms.validators.Required()])
+
+
+class ChangePassForm(wtforms.Form):
+    old_password = wtforms.PasswordField(
+        _('Old password'),
+        [wtforms.validators.Required()],
+        description=_(
+            "Enter your old password to prove you own this account."))
+    new_password = wtforms.PasswordField(
+        _('New password'),
+        [wtforms.validators.Required(),
+         wtforms.validators.Length(min=6, max=30)],
+        id="password")
